db3d0d19e6c882c68c173e94f6f3909f50db6fcb
Akshat Chauhan <akshatchauhan2532@gmail.com>
2024-03-22 17:17:30 +0530

    Updates

---
+++ precache-manifest.013991e27c155ced80fccd1e35a48df2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee6a2c172ec6ddf836456c7b1d5e94c",
+    "url": "/myrobofrii/index.html"
+  },
+  {
+    "revision": "b44383eaeaf87a960146",
+    "url": "/myrobofrii/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "90727e271e1e30abd3d9",
+    "url": "/myrobofrii/static/css/main.db4b3cf4.chunk.css"
+  },
+  {
+    "revision": "b44383eaeaf87a960146",
+    "url": "/myrobofrii/static/js/2.60246997.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/myrobofrii/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "90727e271e1e30abd3d9",
+    "url": "/myrobofrii/static/js/main.9504f965.chunk.js"
+  },
+  {
+    "revision": "8a499f3cfe0e748aabfa",
+    "url": "/myrobofrii/static/js/runtime~main.3de2e337.js"
+  }
+]);
